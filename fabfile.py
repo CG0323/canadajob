6cb5ace95e4f965798195c05823588160452f0cb
cg <wangchun0323@qq.com>
2016-12-04 16:46:00 +0800

    auto commit with fabric

---
+++ fabfile.py
@@ -0,0 +1,36 @@
+#coding=utf-8
+from __future__ import with_statement
+from fabric.api import local, env, settings, abort, run, cd
+from fabric.contrib.console import confirm
+import time
+
+# env.use_ssh_config = True
+env.hosts = ['52.8.218.46']
+env.user = 'ec2-user'
+env.key_filename = '/Users/wangchun/Downloads/development/cg007.pem'
+
+# code_dir='/var/www/deploy-stage'
+# app_dir='/var/www/application'
+repo='https://github.com/CG0323/canadajob.git'
+timestamp="release_%s" % int(time.time() * 1000)
+
+def deploy():
+    push()
+    server_pull()
+
+def push():
+    local("git add --a")
+    local("git commit -m 'auto commit with fabric'")
+    local("git push")
+
+def server_pull():
+    with cd('~/canadajob'):   #cd用于进入某个目录
+        # run('git add --a') 
+        # run('git stash') 
+        run('git pull')  #远程操作用run
+        run('sudo chmod -R 777 ~/canadajob')
+        
+def install_modules():
+    run('source env/bin/activate')
+    run('pip install -r requirement.txt')
+
---
+++ search.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import json
+import datetime,time
+from time import mktime
+
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return int(mktime(obj.timetuple()))
+        return json.JSONEncoder.default(self, obj)
+
+def readPage(text, jobs):
+    soup = BeautifulSoup(text,'lxml')
+    jobElements = soup.find_all("div", class_="job-c")
+    for jobElement in jobElements:
+        job = {}
+
+        titleElement = jobElement.find("a", class_="gojob")
+        link = titleElement["href"]
+        spans = titleElement.find_all("span")
+        jobTitle = ""
+        for span in spans:
+            jobTitle += span.getText()
+        job["title"] = jobTitle
+        job["link"] = "http://neuvoo.ca" + link;
+
+        infoElement = jobElement.find("div", class_="j-info")
+        employer = infoElement.find("span", itemprop="name").getText()
+        job["employer"] = employer
+        address = {}
+        address["city"] = infoElement.find("span", itemprop="addressLocality").getText()
+        address["province"] = infoElement.find("span", itemprop="addressRegion").getText()
+        job["address"] = address
+        dateString = infoElement.find("div", class_="j-date").getText()
+        dateString = dateString.replace(" - ","")
+        dateString = dateString.replace(" ago ","")
+        d1 = datetime.datetime.now()
+        if "d" in dateString:
+            d = int(dateString.replace("d",""))
+            job["date"] = d1 - datetime.timedelta(days=d)
+        elif "h" in dateString:
+            h = int(dateString.replace("h",""))
+            job["date"] = d1 - datetime.timedelta(hours=h)
+        jobs.append(job); 
+
+url = 'http://neuvoo.ca/jobs/?k=.NET+Developer&l=montreal&f=&p=&r=';
+# url = "www.163.com"
+driver = webdriver.PhantomJS()
+driver.get(url)
+text = driver.page_source
+jobs = []
+readPage(text, jobs)
+print jobs
