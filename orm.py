58de50719b0cb00b8fdb2038b70b7c5cf0f2fabe
cg <wangchun0323@qq.com>
2016-12-05 21:45:05 +0800

    auto commit with fabric

--- orm.py
+++ orm.py
@@ -1,28 +1,31 @@
 #!./env/bin/python
+VARCHAR(20)
 import pymysql.cursors
 
-# Connect to the database
-db = pymysql.connect(host='localhost',
-                             user='cg',
-                             password='088583-Salahdin',
-                             db='canadajob',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
-
 def create_tables():
-    # 使用 cursor() 方法创建一个游标对象 cursor
-    cursor = db.cursor()
+    # 执行sql语句
+    try:
+    # Connect to the database
+        connection = pymysql.connect(host='localhost',
+                                    user='cg',
+                                    password='088583-Salahdin',
+                                    db='canadajob',
+                                    charset='utf8mb4',
+                                    cursorclass=pymysql.cursors.DictCursor)
+        with connection.cursor() as cursor:
+        # 使用预处理语句创建表
+        sql = """CREATE TABLE DRAFT IF NOT EXIST (
+                POSTAT DATETIME,
+                TITLE  VARCHAR(45),
+                EMPLOYEE  VARCHAR(20),
+                PROVINCE VARCHAR(20),  
+                CITY VARCHAR(20), 
+                URL VARCHAR(100) )"""
 
-    # 使用预处理语句创建表
-    sql = """CREATE TABLE DRAFT IF NOT EXIST (
-            POSTAT DATETIME,
-            TITLE  VARCHAR(45),
-            EMPLOYEE  VARCHAR(20),
-            PROVINCE VARCHAR(20),  
-            CITY VARCHAR(20), 
-            URL VARCHAR(100) )"""
+        cursor.execute(sql)
+        # 没有设置默认自动提交，需要主动提交，以保存所执行的语句
+        connection.commit()
+    
+    finally:
+        connection.close();
 
-    cursor.execute(sql)
-
-    # 关闭数据库连接
-    db.close()
