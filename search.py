e84b40aed6cf10095034315ae33f8e947819514e
cg <wangchun0323@qq.com>
2016-12-06 07:41:40 +0800

    auto commit with fabric

--- search.py
+++ search.py
@@ -54,4 +54,4 @@ text = driver.page_source
 jobs = []
 readPage(text, jobs)
 create_draft_table()
-add_draft(jobs[0]["date"], jobs[0]["title"], jobs[0]["employer"], jobs[0]["city"], jobs[0]["province"], jobs[0]["url"])
+add_draft(jobs[0]["date"], jobs[0]["title"], jobs[0]["employer"], jobs[0]["address"]["city"], jobs[0]["address"]["province"], jobs[0]["url"])
