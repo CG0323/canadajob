df09a61f73e6720711e10a39db7a61dd85da372d
cg <wangchun0323@qq.com>
2016-12-06 22:49:43 +0800

    auto commit with fabric

--- orm.py
+++ orm.py
@@ -112,7 +112,7 @@ def get_drafts_by_province(province):
         with connection.cursor() as cursor:
             sql = "SELECT * FROM draft WHERE province = %s AND refined=%s"   
             
-            cursor.execute(sql,(province,False,))
+            cursor.execute(sql,("Quebec",False,))
 
             cursor.execute(sql,data)
             
