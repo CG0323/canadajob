46e19194282c212a67279489a21d3cdd020e2c6b
cg <wangchun0323@qq.com>
2016-12-06 22:40:34 +0800

    auto commit with fabric

--- orm.py
+++ orm.py
@@ -81,6 +81,36 @@ def add_draft(post_at, title, employer, province, city, url):
     finally:
         connection.close();
 
+def get_test():
+    try:
+
+        connection = pymysql.connect(host='localhost',
+                                    user='cg',
+                                    password='088583-Salahdin',
+                                    db='canadajob',
+                                    charset='utf8mb4',
+                                    cursorclass=pymysql.cursors.DictCursor)
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM draft LIMIT 20"   
+            
+
+            cursor.execute(sql)
+            
+            results = cursor.fetchall()
+            for row in results:
+                draft = {}
+                draft["id"]=row[0]
+                draft["post_at"]=row[1]
+                draft["title"]=row[2]
+                draft["employer"]=row[3]
+                draft["city"]=row[4]
+                draft["province"]=row[5]
+                draft["url"]=row[6]
+                drafts.append(draft)
+            return drafts;
+    except:
+        print ("Error: unable to fecth data")
+
 def get_drafts_by_province(province):  
     try:
         drafts = []
@@ -91,10 +121,11 @@ def get_drafts_by_province(province):
                                     charset='utf8mb4',
                                     cursorclass=pymysql.cursors.DictCursor)
         with connection.cursor() as cursor:
-            sql = "SELECT * FROM draft WHERE province = %s and refined = %s"   
+            sql = "SELECT * FROM draft WHERE province = %s AND refined = %s"   
             data = (province,0,)  
 
             cursor.execute(sql,data)
+            
             results = cursor.fetchall()
             for row in results:
                 draft = {}
