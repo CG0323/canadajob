23316740f91fb9c6a6e4d167f19a6ec632c167c8
cg <wangchun0323@qq.com>
2016-12-08 08:56:58 +0800

    auto commit with fabric

--- retrieve_contents.py
+++ retrieve_contents.py
@@ -81,7 +81,7 @@ def retrieve_content(driver,draft):
             text = cleanJoillico(driver.page_source)
         elif rurl.find("neuvoo.ca") != -1:
             print "neuvoo found!"
-            element = WebDriverWait(driver, 10).until(lambda x : x.find_element_by_id("job-container"))
+            element = WebDriverWait(driver, 25).until(lambda x : x.find_element_by_id("job-container"))
             text = cleanNeuvoo(driver.page_source)
         else:
             unknown = True
