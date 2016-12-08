822ee221b233f7eec69cffc5c95e4a887edaa717
cg <wangchun0323@qq.com>
2016-12-08 15:38:08 +0800

    auto commit with fabric

--- schedule.py
+++ schedule.py
@@ -3,15 +3,21 @@ from crontab import CronTab
 
 cron  = CronTab(user=True)
 
-job_daily  = cron.new(command='cd ~/canadajob;source env/bin/activate;./daily_task.py',comment='job_daily')
+job_daily = cron.new(command='sudo su; reboot',comment='job_daily')
 
-job_daily.every_reboot()
-
+job_daily.minute.on(0)
+job_daily.hour.on(0)
 job_daily.enable()
+
+job_reboot  = cron.new(command='cd ~/canadajob;source env/bin/activate;./daily_task.py',comment='job_reboot')
+
+job_reboot.every_reboot()
+
+job_reboot.enable()
 
 job_hourly = cron.new(command='cd ~/canadajob;source env/bin/activate;./hourly_task.py',comment='job_hourly')
 
-job_hourly.minute.on(0)
+job_hourly.minute.on(45)
 job_hourly.hour.during(0,23)
 job_hourly.enable()
 
