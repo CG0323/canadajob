e82a10d1e1ad4000fde006ba5aacb2df19bf48bb
cg <wangchun0323@qq.com>
2016-12-09 19:47:10 +0800

    auto commit with fabric

--- schedule.py
+++ schedule.py
@@ -11,13 +11,13 @@ job_daily.enable()
 
 job_reboot  = cron.new(command='cd ~/canadajob;source env/bin/activate;./daily_task.py',comment='job_reboot')
 
-job_daily.minute.on(2)
+job_daily.minute.on(02)
 job_daily.hour.on(8)
 job_daily.enable()
 
 job_hourly = cron.new(command='cd ~/canadajob;source env/bin/activate;./hourly_task.py',comment='job_hourly')
 
-job_hourly.minute.on(25)
+job_hourly.minute.on(12)
 job_hourly.hour.on(8)
 job_hourly.enable()
 
