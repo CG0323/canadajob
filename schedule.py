501629025f93226ccfebf6a4b8cb8de9349948c4
cg <wangchun0323@qq.com>
2016-12-09 21:02:35 +0800

    auto commit with fabric

--- schedule.py
+++ schedule.py
@@ -3,23 +3,23 @@ from crontab import CronTab
 
 cron  = CronTab(user=True)
 
-job_daily = cron.new(command='sudo su; /sbin/reboot',comment='job_reboot')
+job_reboot = cron.new(command='sudo su; /sbin/reboot',comment='job_reboot')
 
-job_daily.minute.on(2)
-job_daily.hour.on(7)
-job_daily.enable()
-
-job_reboot  = cron.new(command='cd ~/canadajob;source env/bin/activate;./daily_task.py',comment='job_menu')
-
-job_reboot.minute.on(8)
+job_reboot.minute.on(2)
 job_reboot.hour.on(7)
 job_reboot.enable()
 
-job_hourly = cron.new(command='cd ~/canadajob;source env/bin/activate;./hourly_task.py',comment='job_content')
+job_menu  = cron.new(command='cd ~/canadajob;source env/bin/activate;./daily_task.py',comment='job_menu')
 
-job_hourly.minute.on(16)
-job_hourly.hour.on(7)
-job_hourly.enable()
+job_menu.minute.on(8)
+job_menu.hour.on(7)
+job_menu.enable()
+
+job_content = cron.new(command='cd ~/canadajob;source env/bin/activate;./hourly_task.py',comment='job_content')
+
+job_content.minute.on(08)
+job_content.hour.on(8)
+job_content.enable()
 
 cron.write()
 cron.render()
