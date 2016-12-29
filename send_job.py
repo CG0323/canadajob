c3da8b420b4fb43076e8d64d6828627d55312e07
CG0323 <wangchun0323@qq.com>
2016-12-29 20:22:52 +0800

    'auto_commit_with_fabric'

--- send_job.py
+++ send_job.py
@@ -8,7 +8,7 @@ url = 'http://60.205.216.128:8080/api/jobs'
 jobs = get_new_valid_drafts()
 
 for job in jobs:
-    text = get_content_by_draft_id(job.id)["content"].lower()
+    text = get_content_by_draft_id(job["id"])["content"].lower()
     content = {"text": text}
     data = {"readAt": job["read_at"], "postAt": job["post_at"], "title": job["title"], "employer":job["employer"], "province":job["provice"], "city":job["city"], "url":job["rurl"], "content": content}
     r = requests.post(url, json = data)
