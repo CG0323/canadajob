9b0d2c6516bea7181e0c97b3b6a99f4c2d9749ab
CG0323 <wangchun0323@qq.com>
2016-12-29 20:31:58 +0800

    'auto_commit_with_fabric'

--- send_job.py
+++ send_job.py
@@ -10,7 +10,7 @@ jobs = get_new_valid_drafts()
 for job in jobs:
     text = get_content_by_draft_id(job["id"])["content"].lower()
     content = {"text": text}
-    data = {"readAt": job["read_at"], "postAt": job["post_at"], "title": job["title"], "employer":job["employer"], "province":job["provice"], "city":job["city"], "url":job["rurl"], "content": content}
+    data = {"readAt": job["read_at"].strftime('%Y-%m-%d %H:%M:%S'), "postAt": job["post_at"].strftime('%Y-%m-%d %H:%M:%S'), "title": job["title"], "employer":job["employer"], "province":job["provice"], "city":job["city"], "url":job["rurl"], "content": content}
     r = requests.post(url, json = data)
     if r.status_code == requests.codes.ok:
       set_draft_sent(job.id)
