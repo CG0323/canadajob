981126240502a629614aabbe25201fb6a1fce34b
CG0323 <wangchun0323@qq.com>
2016-12-29 22:30:57 +0800

    'auto_commit_with_fabric'

--- send_job.py
+++ send_job.py
@@ -24,7 +24,8 @@ for job in jobs:
     title = title.replace("DeveloperDeveloper","Developer")
     data = {"readAt": read_at, "postAt": job["post_at"].strftime('%Y-%m-%d %H:%M:%S'), "title": title, "employer":job["employer"], "province":job["province"], "city":job["city"], "url":job["rurl"], "content": content}
     r = requests.post(url, json = data)
-    print r.text
+    print "sent job id = " + job["id"]
+    print r.status_code
     set_draft_sent(job["id"])
     #   if r.status_code == requests.codes.ok:
     #     print "successfull sent job " + job["title"]
